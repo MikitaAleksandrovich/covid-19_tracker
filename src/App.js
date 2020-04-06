e33108ffcee691be69ce104e27a956c9052f8deb
Mikita Aleksandrovich <45469739+MikitaAleksandrovich@users.noreply.github.com>
2020-04-06 18:48:50 +0300

    Add initial project with right structure

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import { Cards, Chart, CountryPicker } from './components';
+import styles from './App.module.css';
+
+class App extends Component {
+    render() {
+        return(
+            <div className={styles.container}>
+                <Cards/>
+                <CountryPicker />
+                <Chart />
+            </div>
+        )
+    }
+};
+
+export default App;
