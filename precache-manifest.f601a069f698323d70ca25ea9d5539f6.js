e3890741ac6c7e6d056953c258010bbf471f90a1
Mikita Aleksandrovich <45469739+MikitaAleksandrovich@users.noreply.github.com>
2020-04-09 19:06:32 +0300

    Updates

---
+++ precache-manifest.f601a069f698323d70ca25ea9d5539f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f0ea87e0900a7fdb740ca67d8690c8a",
+    "url": "/covid-19_tracker/index.html"
+  },
+  {
+    "revision": "1a2a5d1fdbbd1a225ace",
+    "url": "/covid-19_tracker/static/css/main.d86d1766.chunk.css"
+  },
+  {
+    "revision": "4bc8290432d48af9374e",
+    "url": "/covid-19_tracker/static/js/2.413c1143.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19_tracker/static/js/2.413c1143.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a2a5d1fdbbd1a225ace",
+    "url": "/covid-19_tracker/static/js/main.48c93840.chunk.js"
+  },
+  {
+    "revision": "d91bf3f34a2a224bdfca",
+    "url": "/covid-19_tracker/static/js/runtime-main.60b43f9f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid-19_tracker/precache-manifest.2377a1ee3cb9fda1d43a087f0d4769d4.js"
+  "/covid-19_tracker/precache-manifest.f601a069f698323d70ca25ea9d5539f6.js"
 );
 
 self.addEventListener('message', (event) => {
